49360b57f2fc30fde8603ffaa4517bb42d893228
Busayo Folorunso <busayo.folorunso@datawizable.com>
2026-03-18 12:53:04 +0100

    correction of url

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://risenonchain.com",
-        "https://www.risenonchain.com",
+        "https://risenonchain.net",
+        "https://www.risenonchain.net",
     ],
     allow_credentials=True,
     allow_methods=["*"],
